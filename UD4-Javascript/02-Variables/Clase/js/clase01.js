865e11e357a9cda2203af3cad1fa48509ef7038c
arodriguezgim <arodriguezg@iesch.org>
2024-11-15 14:17:45 +0100

    Fin clase viernes

---
+++ UD4-Javascript/02-Variables/Clase/js/clase01.js
@@ -0,0 +1,13 @@
+/* Ejercicio 5 Comparación de Edades */
+
+let edad1 = 56;
+
+let edad2 = 89;
+
+if ( edad1 < edad2 ) {
+    // Esto solo se ejecutará si edad1 es mayor que edad2
+    console.log("He entrado por aqui. ")
+} 
+
+console.log("Fin del programa");
+
