151f65877463beaf0be43ee68df76401f0f6e14c
arodriguezgim <arodriguezg@iesch.org>
2024-11-25 10:16:33 +0100

    Fin clase Lunes

---
+++ UD4-Javascript/04-Funciones/Dia01/js/funciones.js
@@ -0,0 +1,18 @@
+function sumar( num1, num2){
+    let resultado = num1 + num2;
+    alert("La suma es "+ resultado);
+}
+function restar( num1, num2){
+    let resultado = num1 - num2;
+    alert("La resta es " + resultado );
+}
+
+function multiplicar( num1, num2){
+    let resultado = num1 * num2;
+    alert("La multiplicación es " + resultado );
+}
+function dividir( num1, num2){
+    let resultado = num1 / num2;
+    alert("La división es " + resultado );
+}
+
